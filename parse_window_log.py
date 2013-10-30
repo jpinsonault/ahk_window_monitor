a7ab89ffc9dd8260f25aca98a7bbc8c6e573e93e
Joe Pinsonault <joe.pinsonault@gmail.com>
2013-10-30 11:16:41 -0700

    Delinted python scripts

--- parse_window_log.py
+++ parse_window_log.py
@@ -3,19 +3,21 @@ from pprint import pprint
 
 from AHKLogParser import AHKLogParser
 
-def parse_args():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('log_file')
 
-	return parser.parse_args()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('log_file')
+
+    return parser.parse_args()
+
 
 def main(args):
-	print("Parsing...")
-	parser = AHKLogParser(args.log_file)
-	
-	pprint([line.get_dict()["duration"] for line in parser.activity_log if line.get_dict()["active"] == False])
+    print("Parsing...")
+    parser = AHKLogParser(args.log_file)
 
-	print("{} lines in log file, {} distinct activities".format(len(parser.log_dict), len(parser.activity_log)))
+    pprint([line.get_dict()["duration"] for line in parser.activity_log if line.get_dict()["active"] == False])
+
+    print("{} lines in log file, {} distinct activities".format(len(parser.log_dict), len(parser.activity_log)))
 
 if __name__ == '__main__':
-	main(parse_args())
+    main(parse_args())
